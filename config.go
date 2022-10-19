150b7296f414c121a1c00d7b51713b0abf5f270b
jiurongzhao <1053668864@qq.com>
2022-10-19 21:14:55 +0800

    feat: print message

--- config.go
+++ config.go
@@ -24,7 +24,7 @@ var resource gjson.Result
 func Register(name string, adapter Adapter) {
 	_, ok := adapters[name]
 	if ok {
-		panic(fmt.Errorf("适配器 [%s] 已经装载，请勿重复加载", name))
+		panic(fmt.Errorf("adapter [%s] already register", name))
 	}
 	adapters[name] = adapter
 }
@@ -32,13 +32,13 @@ func Register(name string, adapter Adapter) {
 func InitInstance(name string, filename string) error {
 	adapter, ok := adapters[name]
 	if !ok {
-		return fmt.Errorf("找不到适配器 [%s] 请确定已经装载", name)
+		return fmt.Errorf("not found [%s] adapter", name)
 	}
 
 	if instance != nil {
-		fmt.Printf("配置适配器已经存在，将使用 [%s] 替换\n", name)
+		fmt.Printf("[Bootstrap-Config]  Instance [%s] will be replaced. \n", name)
 	}
-	fmt.Printf("开始使用配置 [%s] 载入适配器\n", filename)
+	fmt.Printf("[Bootstrap-Config]  Init [%s] adapter. \n", filename)
 	if newInstance, err := adapter.Load(filename); err != nil {
 		return err
 	} else {
@@ -48,8 +48,6 @@ func InitInstance(name string, filename string) error {
 	}
 }
 
-// 获取配置值，可按照需要的类型进行安全的转换
-// 如: value.Int()
 func Get(key string) (*gjson.Result, bool) {
 	if res := cache[key]; res != nil {
 		return res, true
@@ -62,7 +60,6 @@ func Get(key string) (*gjson.Result, bool) {
 	return nil, false
 }
 
-// 将配置解析为结构体
 func Resolve(prefix string, p interface{}) error {
 	if prefix == "" {
 		return json.Unmarshal([]byte(resource.String()), p)
